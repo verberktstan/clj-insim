a700b11072d8e9ff5db4d422009669dfa2c6e7a0
verberktstan <stan@brightin.nl>
2018-12-01 20:31:24 +0100

    Refactor core parser and dispatcher

--- src/clj_insim/core.clj
+++ src/clj_insim/core.clj
@@ -7,50 +7,34 @@
             [clj-insim.util :as util])
   (:import [java.nio ByteBuffer]))
 
-(defn print-keepalive-dispatch [protocol]
-  (fn [packet]
-    (let [m (parse-packet packet protocol)]
-      (prn m)
-      (packets/is-tiny))))
+(def dispatchers nil)
 
-(defn npl-dispatch [packet]
-  (let [{:keys [handicap-mass number-player uniq-connection-id]}
-         (parse-packet packet (is-protocols :npl))
-        jrr-map {:uniq-connection-id uniq-connection-id :player-id 0}]
-    (if (< handicap-mass 15)
-      (packets/is-jrr (assoc jrr-map :jrr-action (enums/jrr-action :reject)))
-      (packets/is-tiny))))
+(defn dispatch [{:keys [type] :as incoming}]
+  (let [f (type dispatchers)]
+    (do (prn incoming)
+      (if f
+        (f incoming)
+        (packets/is-tiny)))))
 
-(def type-dispatch
-  {:ver (fn [packet]
-          (let [m (parse-packet packet (is-protocols :ver))]
-            (println (str m))
-            (packets/is-mst "Hello from clj-insim!")))
-   :tiny (print-keepalive-dispatch (is-protocols :tiny))
-   :mso (print-keepalive-dispatch (is-protocols :mso))
-   :sta (print-keepalive-dispatch (is-protocols :sta))
-   :flg (print-keepalive-dispatch (is-protocols :flg))
-   :csc (print-keepalive-dispatch (is-protocols :csc))
-   :npl npl-dispatch
-   })
+(defn parse [type-key packet]
+  (parse-packet packet (is-protocols type-key)))
 
 ;; Simple hander prints the type of packet received and returns a IS_TYNI/none packet to maintain connection
-(defn simple-handler [packet]
+(defn handler [packet]
   (let [[type] packet
         type-key (enums/isp-key (int type))
-        f (type-dispatch type-key)]
+        incoming (parse type-key packet)]
     (do
-      (println (str "=== Received " (name type-key) " packet from LFS ==="))
-      (if f ; Execute dispatch OR return keepalive packet
-        (f packet)
+      (println "=== Received " (name type-key) " packet from LFS ===")
+      (if incoming
+        (dispatch incoming)
         (packets/is-tiny)))))
 
 (comment
   ;; Start a tcp client with simple-handler
-                                        ;  (def simple-server (serve HOST PORT simple-handler))
-  (def simple-server (serve simple-handler))
+  (def simple-server (serve handler))
   ;; To stop the client
   (reset! simple-server false)
 
-  (def npl-packet {:uniq-connection-id 0, :driver-model 30, :player-type 0, :spare-3 0, :car-name "FZR", :setup-flags 0, :number-player 1, :spare-2 0, :handicap-mass 0, :type :npl, :player-id 2, :skin-namE "ARCHLINUX1", :passenger 0, :handicap-restriction 0, :reqi 0, :license-plate "", :flags (72 18), :player-name "Van Sterberkt", :tyres (2 2 1 1), :spare (0 0 0 0)})
+  (:test nil)
 )
