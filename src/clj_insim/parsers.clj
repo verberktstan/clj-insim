7c2176e029364611e936958c38b68a6095cc9768
verberktstan <stan@brightin.nl>
2018-12-03 19:40:37 +0100

    Clean up parsers ns

--- src/clj_insim/parsers.clj
+++ src/clj_insim/parsers.clj
@@ -28,80 +28,86 @@
   (into [] (map parse-protocol-map c)))
 
 (def ^{:private true} is-protocols
-  {:csc (make-protocol
-         [{:key :type :type :type} {:key :reqi} {:key :player-id}
-          {:key :spare} {:key :csc-action} {:key :spare-2} {:key :spare-3}])
-   :flg (make-protocol
-         [{:key :type :type :type} {:key :reqi} {:key :player-id}
-          {:key :off-on} {:key :flag} {:key :car-behind} {:key :spare-3}])
-   :ism (make-protocol
-         [{:key :type :type :type} {:key :reqi} {:key :zero}
-          {:key :host} {:key :spare-1} {:key :spare-2} {:key :spare-3}
-          {:key :host-name :type :string :length 32}])
-   :mso (make-protocol
-         [;{:key :size}
-          {:key :type :type :type}
-          {:key :reqi}
-          {:key :zero}
-          {:key :uniq-connection-id}
-          {:key :player-id}
-          {:key :user-type}
-          {:key :text-start}
-          {:key :message :type :string :length 128}])
+  {
+   ;; IS_FLG
+   :flg [{:key :type :type :type} {:key :reqi} {:key :player-id}
+         {:key :off-on}
+         {:key :flag}
+         {:key :car-behind}
+         {:key :spare-3}]
 
-   ;;;;; IS_NCN - New ConnectioN ;;;;;
-   :ncn (make-protocol
-         [{:key :type :type :type} {:key :reqi} {:key :uniq-connection-id}
-          {:key :user-name :type :string :length 24}
-          {:key :player-name :type :string :length 24}
-          {:key :admin} {:key :total} {:key :flags} {:key :spare}])
+   ;; IS_ISM
+   :ism [{:key :type :type :type} {:key :reqi} {:key :zero}
+         {:key :host}
+         {:key :spare-1}
+         {:key :spare-2}
+         {:key :spare-3}
+         {:key :host-name :type :string :length 32}]
 
-   :npl (make-protocol
-         [;{:key :size}
-          {:key :type :type :type}
-          {:key :reqi}
-          {:key :player-id}
-          {:key :uniq-connection-id}
-          {:key :player-type}
-          {:key :flags :type :word}
-          {:key :player-name :type :string :length 24}
-          {:key :license-plate :type :string :length 8}
-          {:key :car-name :type :string :length 4}
-          {:key :skin-name :type :string :length 16}
-          {:key :tyres :type :bytes :length 4}
-          {:key :handicap-mass}
-          {:key :handicap-restriction}
-          {:key :driver-model}
-          {:key :passenger}
-          {:key :spare :type :int}
-          {:key :setup-flags}
-          {:key :number-player}
-          {:key :spare-2}
-          {:key :spare-3}])
-   :tiny (make-protocol
-          [;{:key :size}
-           {:key :type :type :type}
-           {:key :reqi}
-           {:key :subt-type :type :tiny-subtype}])
-   :sta (make-protocol
-         [{:key :type :type :type} {:key :reqi} {:key :zero}
-          {:key :replay-speed :type :float} {:key :flags :type :word}
-          {:key :ingame-cam} {:key :viewed-player-id}
-          {:key :num-players-in-race} {:key :num-connections}
-          {:key :num-finished} {:key :race-in-progress}
-          {:key :qualify-minutes} {:key :race-laps}
-          {:key :spare-2} {:key :spare-3}
-          {:key :track :type :string :length 6}
-          {:key :weather} {:key :wind}])
-   :ver (make-protocol
-         [;{:key :size}
-          {:key :type :type :type} ; The first byte is parsed as {:type :some-key}
-          {:key :reqi} ; The second byte is parsed as {:reqi int}
-          {:key :zero}
-          {:key :version :type :string :length 8} ; The next 8 bytes are parsed as {:version "string"}
-          {:key :product :type :string :length 6} ; The next 6 bytes are parsed as {:product "string"}
-          {:key :insim-version} ; {:insim-version int}
-          {:key :spare}])})
+   ;; IS_MSO
+   :mso [{:key :type :type :type} {:key :reqi} {:key :zero}
+         {:key :uniq-connection-id}
+         {:key :player-id}
+         {:key :user-type}
+         {:key :text-start}
+         {:key :message :type :string :length 128}]
+
+   ;; IS_NCN - New ConnectioN
+   :ncn [{:key :type :type :type} {:key :reqi} {:key :uniq-connection-id}
+         {:key :user-name :type :string :length 24}
+         {:key :player-name :type :string :length 24}
+         {:key :admin}
+         {:key :total}
+         {:key :flags}
+         {:key :spare}]
+
+   ;; IS_NPL
+   :npl [{:key :type :type :type} {:key :reqi} {:key :player-id}
+         {:key :uniq-connection-id}
+         {:key :player-type}
+         {:key :flags :type :word}
+         {:key :player-name :type :string :length 24}
+         {:key :license-plate :type :string :length 8}
+         {:key :car-name :type :string :length 4}
+         {:key :skin-name :type :string :length 16}
+         {:key :tyres :type :bytes :length 4}
+         {:key :handicap-mass}
+         {:key :handicap-restriction}
+         {:key :driver-model}
+         {:key :passenger}
+         {:key :spare :type :int}
+         {:key :setup-flags}
+         {:key :number-player}
+         {:key :spare-2}
+         {:key :spare-3}]
+
+   ;; IS_TINY
+   :tiny [{:key :type :type :type} {:key :reqi} {:key :subt-type :type :tiny-subtype}]
+
+   ;; IS_STA
+   :sta [{:key :type :type :type} {:key :reqi} {:key :zero}
+         {:key :replay-speed :type :float}
+         {:key :flags :type :word}
+         {:key :ingame-cam}
+         {:key :viewed-player-id}
+         {:key :num-players-in-race}
+         {:key :num-connections}
+         {:key :num-finished}
+         {:key :race-in-progress}
+         {:key :qualify-minutes}
+         {:key :race-laps}
+         {:key :spare-2}
+         {:key :spare-3}
+         {:key :track :type :string :length 6}
+         {:key :weather}
+         {:key :wind}]
+
+   ;; IS_VER
+   :ver [{:key :type :type :type} {:key :reqi} {:key :zero}
+         {:key :version :type :string :length 8}
+         {:key :product :type :string :length 6}
+         {:key :insim-version}
+         {:key :spare}]})
 
 
 (defn- parse-bytes
@@ -116,6 +122,6 @@
           (assoc key (cast c1))))))
 
 (defn parse [type-key packet]
-  (let [protocol (is-protocols type-key)]
+  (let [protocol (make-protocol (is-protocols type-key))]
     (when protocol
       (reduce parse-bytes {:coll packet} protocol))))
