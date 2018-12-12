af5b3a3ad0a23e0181621d21f1631b8793f81ae8
verberktstan <stan@brightin.nl>
2018-12-12 21:29:54 +0100

    Add description and github url to project.clj

--- project.clj
+++ project.clj
@@ -1,7 +1,6 @@
 (defproject clj-insim "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [clj-sockets "0.1.0"]])
+  :description "A Clojure library designed to interface with Live For Speed InSim."
+  :url "https://github.com/verberktstan/clj-insim"
+  :license {:name "Unlicense"
+            :url "http://unlicense.org"}
+  :dependencies [[org.clojure/clojure "1.8.0"]])
