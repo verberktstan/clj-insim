82fe7b9683a00df3d53b0795c0ee4458fca7def2
verberktstan <stan@brightin.nl>
2018-12-13 19:20:44 +0100

    Add pipeline test

--- test/clj_insim/core_test.clj
+++ test/clj_insim/core_test.clj
@@ -1,7 +1,9 @@
 (ns clj-insim.core-test
   (:require [clojure.test :refer :all]
-            [clj-insim.core :refer :all]))
+            [clj-insim.parsers :refer [parse]]
+            [clj-insim.packets :as packets]))
 
-(deftest a-test
-  (testing "Jihaa!"
-    (is (= 1 1))))
+(deftest pipeline-test
+  (testing "Parsing and constructing packets"
+    (is (= (parse (drop 1 (packets/is-tiny)))
+           {:type :tiny, :reqi 0, :subt-type :none}))))
