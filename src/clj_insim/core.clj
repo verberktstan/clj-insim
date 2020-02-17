5e532ae425f5f5174e3a857c11e3f911e42355d9
verberktstan <stan@brightin.nl>
2020-02-17 22:30:57 +0100

    Completele rewrite core ns

--- src/clj_insim/core.clj
+++ src/clj_insim/core.clj
@@ -16,56 +16,51 @@
 (defn- pop! [queue]
   (swap! queue pop))
 
-(defn- reset-queue! [queue-key]
-  (when-let [queue (get QUEUES queue-key)]
-    (clojure.core/reset! queue (clojure.lang.PersistentQueue/EMPTY))))
+(defn- reset-queue! [queue]
+  (reset! queue (clojure.lang.PersistentQueue/EMPTY)))
 
 (defn- reset-queues! []
-  (doseq [queue-key (keys QUEUES)]
-    (reset-queue! queue-key)))
+  (doseq [queue (vals QUEUES)] (reset-queue! queue)))
 
 (defn- ->queue
   "Put packet(s) p on a queue."
-  [queue-key p]
-  (when-let [queue (get QUEUES queue-key)]
-    (cond
-      (s/valid? ::packet/model p)
-      (swap! queue conj p)
+  [queue p]
+  (cond
+    (s/valid? ::packet/model p)
+    (swap! queue conj p)
 
-      (s/valid? (s/coll-of ::packet/model) p)
-      (apply swap! queue conj p)
+    (s/valid? (s/coll-of ::packet/model) p)
+    (apply swap! queue conj p)
 
-      :else
-      (println
-       (or (s/explain-data ::packet/model p)
-           (s/explain-data (s/coll-of ::packet/model) p))))))
+    :else
+    (println
+     (or (s/explain-data ::packet/model p)
+         (s/explain-data (s/coll-of ::packet/model) p)))))
 
-(defn- input-stream->in-queue!
+(defn- read-packets!
   "Read a single packet from the input-stream and enqueue it (on the input queue)"
-  [input-stream]
+  [input-stream queue]
   (while (pos? (.available input-stream))
-    (->queue :in-queue (packet/read input-stream))))
+    (->queue queue (packet/read input-stream))))
 
 (defn- dispatch!
   "Call dispatch-fn on all packets (in input queue)
   and enqueue the result (on the output queue)"
-  [dispatch-fn]
-  (let [queue (:in-queue QUEUES)]
-    (while (seq @queue)
-      (let [packet (peek @queue)]
-        (pop! queue)
-        (when (s/valid? ::packet/model packet)
-          (->queue :out-queue (dispatch-fn packet)))))))
+  [in-queue dispatch-fn out-queue]
+  (while (seq @in-queue)
+    (let [packet (peek @in-queue)]
+      (pop! in-queue)
+      (when (s/valid? ::packet/model packet)
+        (->queue out-queue (dispatch-fn packet))))))
 
-(defn- out-queue->output-stream!
+(defn- write-packets!
   "Write all queued packets to the output-stream (if any)."
-  [output-stream]
-  (let [queue (:out-queue QUEUES)]
-    (when-let [packets (->> (seq @queue)
-                            (keep identity)
-                            seq)]
-      (reset-queue! :out-queue)
-      (packet/write output-stream packets))))
+  [out-queue output-stream]
+  (when-let [packets (->> (seq @out-queue)
+                          (keep identity)
+                          seq)]
+    (reset-queue! out-queue)
+    (packet/write output-stream packets)))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; State
@@ -80,7 +75,7 @@
   "Enqueue packet (or vector of packets) p to the queue.
   p must be associative (a hash-map or a vector)."
   [p]
-  (->queue :out-queue p))
+  (->queue (:out-queue QUEUES) p))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Dispatching packets
@@ -103,23 +98,26 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+(defn- make-system [{:keys [in-queue out-queue]} init-packet]
+  {:in-queue (doto in-queue (reset-queue!))
+   :out-queue (doto out-queue (reset-queue!) (->queue init-packet))})
+
 (defn client
   "Opens a tcp socket and reads packets from input stream to in-queue,
   and writes packets from out-queue to output stream."
   ([]
    (client nil))
   ([{:keys [host port sleep-interval dispatch-fn]}]
-   (let [running (atom true)]
-     (reset-queues!)
-     (enqueue! (packets/insim-init))
+   (let [running (atom true)
+         {:keys [in-queue out-queue]} (make-system QUEUES (packets/insim-init))]
      (future
        (with-open [socket (Socket. (or host "127.0.0.1") (or port 29999))
                    output-stream (io/output-stream socket)
                    input-stream (io/input-stream socket)]
          (while @running
-           (input-stream->in-queue! input-stream)
-           (dispatch! dispatch-fn)
-           (out-queue->output-stream! output-stream)
+           (read-packets! input-stream in-queue)
+           (dispatch! in-queue dispatch-fn out-queue)
+           (write-packets! out-queue output-stream)
            (Thread/sleep (or sleep-interval 100)))))
      running)))
 
