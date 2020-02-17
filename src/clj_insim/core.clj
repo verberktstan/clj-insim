f6ad85b60266821618bec96728241ed12614081a
verberktstan <stan@brightin.nl>
2020-02-17 21:53:47 +0100

    Move Queue logic to core ns

--- src/clj_insim/core.clj
+++ src/clj_insim/core.clj
@@ -3,13 +3,84 @@
             [clj-insim.codecs :as codecs]
             [clj-insim.packets :as packets]
             [clj-insim.models.packet :as packet]
-            [clj-insim.queues :refer [enqueue!] :as queues]
             [clojure.java.io :as io]
+            [clojure.spec.alpha :as s]
             [marshal.core :as m])
+  (:refer-clojure :exclude [pop!])
   (:import [java.net Socket]))
+
+(defonce ^:private QUEUES
+  {:in-queue (atom (clojure.lang.PersistentQueue/EMPTY))
+   :out-queue (atom (clojure.lang.PersistentQueue/EMPTY))})
+
+(defn- pop! [queue]
+  (swap! queue pop))
+
+(defn- reset-queue! [queue-key]
+  (when-let [queue (get QUEUES queue-key)]
+    (clojure.core/reset! queue (clojure.lang.PersistentQueue/EMPTY))))
+
+(defn- reset-queues! []
+  (doseq [queue-key (keys QUEUES)]
+    (reset-queue! queue-key)))
+
+(defn- ->queue
+  "Put packet(s) p on a queue."
+  [queue-key p]
+  (when-let [queue (get QUEUES queue-key)]
+    (cond
+      (s/valid? ::packet/model p)
+      (swap! queue conj p)
+
+      (s/valid? (s/coll-of ::packet/model) p)
+      (apply swap! queue conj p)
+
+      :else
+      (println
+       (or (s/explain-data ::packet/model p)
+           (s/explain-data (s/coll-of ::packet/model) p))))))
+
+(defn- input-stream->in-queue!
+  "Read a single packet from the input-stream and enqueue it (on the input queue)"
+  [input-stream]
+  (while (pos? (.available input-stream))
+    (->queue :in-queue (packet/read input-stream))))
+
+(defn- dispatch!
+  "Call dispatch-fn on all packets (in input queue)
+  and enqueue the result (on the output queue)"
+  [dispatch-fn]
+  (let [queue (:in-queue QUEUES)]
+    (while (seq @queue)
+      (let [packet (peek @queue)]
+        (pop! queue)
+        (when (s/valid? ::packet/model packet)
+          (->queue :out-queue (dispatch-fn packet)))))))
+
+(defn- out-queue->output-stream!
+  "Write all queued packets to the output-stream (if any)."
+  [output-stream]
+  (let [queue (:out-queue QUEUES)]
+    (when-let [packets (->> (seq @queue)
+                            (keep identity)
+                            seq)]
+      (reset-queue! :out-queue)
+      (packet/write output-stream packets))))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; State
 
 (defonce version (atom nil))
 (defonce state (atom nil))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Public functions
+
+(defn enqueue!
+  "Enqueue packet (or vector of packets) p to the queue.
+  p must be associative (a hash-map or a vector)."
+  [p]
+  (->queue :out-queue p))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; Dispatching packets
@@ -39,16 +110,16 @@
    (client nil))
   ([{:keys [host port sleep-interval dispatch-fn]}]
    (let [running (atom true)]
-     (queues/reset!)
+     (reset-queues!)
      (enqueue! (packets/insim-init))
      (future
        (with-open [socket (Socket. (or host "127.0.0.1") (or port 29999))
                    output-stream (io/output-stream socket)
                    input-stream (io/input-stream socket)]
          (while @running
-           (queues/read! input-stream)
-           (queues/dispatch! dispatch-fn)
-           (queues/write! output-stream)
+           (input-stream->in-queue! input-stream)
+           (dispatch! dispatch-fn)
+           (out-queue->output-stream! output-stream)
            (Thread/sleep (or sleep-interval 100)))))
      running)))
 
