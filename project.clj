e11b8444f3f37d80fca564f8381fb0f14769cf66
verberktstan <stan@brightin.nl>
2020-02-20 18:48:42 +0100

    Prepare for 0.1.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-insim "0.1.8-SNAPSHOT"
+(defproject clj-insim "0.1.9-SNAPSHOT"
   :description "A Clojure library designed to interface with Live For Speed InSim."
   :url "https://github.com/verberktstan/clj-insim"
   :license {:name "Unlicense"
