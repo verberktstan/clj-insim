87e061d61f047ab11a7006c766ef4c623a0b05cd
verberktstan <stan@brightin.nl>
2021-06-15 10:12:38 +0200

    Refactor parsing, add codecs and parsers, refactor client

--- src/clj_insim/codecs.clj
+++ src/clj_insim/codecs.clj
@@ -8,42 +8,64 @@
    :header/request-info m/ubyte
    :header/data m/ubyte))
 
+(def ^:private SMALL
+  ;; NLI, SSG & SSP use the default `:body/interval` codec
+  {:alc [:body/cars m/uint32]
+   :lcs [:body/switches m/uint32]
+   :rtp [:body/time m/uint32]
+   :stp [:body/number m/uint32]
+   :tms [:body/stop m/uint32]
+   :vta [:body/action m/uint32]})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; The values in the `body` map are functions that return a marshal struct.
+;; Because in some cases we want to change the body codec based on the header
+;; type/data. Take a look at `:small` for an example.
+
 (def body
-  {:isi
-   (m/struct
-    :body/udp-port m/ushort
-    :body/is-flags m/ushort
-    :body/insim-version m/ubyte
-    :body/prefix m/ubyte
-    :body/interval m/ushort
-    :body/admin (m/ascii-string 16)
-    :body/iname (m/ascii-string 16))
+  {:cch
+   (fn [_]
+     (m/struct
+      :body/camera m/ubyte
+      :body/spare (m/ascii-string 3)))
+
+   :isi
+   (fn [_]
+     (m/struct
+      :body/udp-port m/ushort
+      :body/is-flags m/ushort
+      :body/insim-version m/ubyte
+      :body/prefix m/ubyte
+      :body/interval m/ushort
+      :body/admin (m/ascii-string 16)
+      :body/iname (m/ascii-string 16)))
 
    :ver
-   (m/struct
-    :body/version (m/ascii-string 8)
-    :body/product (m/ascii-string 6)
-    :body/insim-version m/ubyte
-    :body/spare m/ubyte)
+   (fn [_]
+     (m/struct
+      :body/version (m/ascii-string 8)
+      :body/product (m/ascii-string 6)
+      :body/insim-version m/ubyte
+      :body/spare m/ubyte))
 
    :small
-   (m/struct
-    :body/update-interval m/uint32)
+   (fn [{:header/keys [data]}]
+     (apply m/struct (get SMALL data [:body/interval m/uint32])))
 
    :sta
-   (m/struct
-    :body/replay-speed m/float
-    :body/flags m/ushort
-    :body/in-game-cam m/ubyte
-    :body/view-player-id m/ubyte
-    :body/num-players m/ubyte
-    :body/num-connections m/ubyte
-    :body/num-finished m/ubyte
-    :body/race-in-progress m/ubyte
-    :body/qualify-minutes m/ubyte
-    :body/race-laps m/ubyte
-    :body/spare2 m/ubyte
-    :body/spare3 m/ubyte
-    :body/track (m/ascii-string 6)
-    :body/weather m/ubyte
-    :body/wind m/ubyte)})
+   (fn [_]
+     (m/struct
+      :body/replay-speed m/float
+      :body/flags m/ushort
+      :body/in-game-cam m/ubyte
+      :body/view-player-id m/ubyte
+      :body/num-players m/ubyte
+      :body/num-connections m/ubyte
+      :body/num-finished m/ubyte
+      :body/race-in-progress m/ubyte
+      :body/qualify-minutes m/ubyte
+      :body/race-laps m/ubyte
+      :body/spare (m/ascii-string 2)
+      :body/track (m/ascii-string 6)
+      :body/weather m/ubyte
+      :body/wind m/ubyte))})
--- src/clj_insim/parse.clj
+++ src/clj_insim/parse.clj
@@ -2,64 +2,102 @@
   (:require [clj-insim.flags :as flags]
             [clj-insim.utils :as u]))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Private parser functions
+
+(defn- parse-race-laps [rl]
+  (cond
+    (zero? rl) {:practice true}
+    (< rl 100) {:laps rl}
+    (< rl 191) {:laps (-> rl (- 100) (* 10) (+ 100))}
+    (> rl 190) {:hours (- rl 190)}))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Private parsing data
+
 (def ^:private TYPES
-  [:none :isi :ver :tiny :small :sta :sch :sfp :scc :cpp :ism :mso :iii :mst :mtc :mod :vtn :rst :ncn
-   :cnl :cpr :npl :plp :pll :lap :spx :pit :psf :pla :cch :pen :toc :flg :pfl :fin :res :reo :nlp
-   :mci :msx :msl :crs :bfn :axi :axo :btn :btc :btt :rip :ssh :con :obh :hlv :plc :axm :acr :hcp
-   :nci :jrr :uco :oco :ttc :slc :csc :cim])
+  [:none :isi :ver :tiny :small :sta :sch :sfp :scc :cpp :ism :mso :iii :mst :mtc
+   :mod :vtn :rst :ncn :cnl :cpr :npl :plp :pll :lap :spx :pit :psf :pla :cch
+   :pen :toc :flg :pfl :fin :res :reo :nlp :mci :msx :msl :crs :bfn :axi :axo
+   :btn :btc :btt :rip :ssh :con :obh :hlv :plc :axm :acr :hcp :nci :jrr :uco :oco
+   :ttc :slc :csc :cim])
 
 (def ^:private DATA
   {:small
    [:none :ssp :ssg :vta :tms :stp :rtp :nli :alc :lcs]
 
    :tiny
-   [:none :ver :close :ping :reply
-    :vtc  :scp :sst   :gth  :mpe
-    :ism  :ren :clr   :ncn  :npl
-    :res  :nlp :mci   :reo  :rst
-    :axi  :axc :rip   :nci  :alc
-    :axm  :slc]
+   [:none :ver :close :ping :reply :vtc :scp :sst :gth :mpe :ism :ren :clr :ncn
+    :npl :res :nlp :mci :reo :rst :axi :axc :rip :nci :alc :axm :slc]
 
    :ttc
    [:none :sel :sel-start :sel-stop]})
 
 (def ^:private STA_FLAGS
-  [:game :replay :paused :shift-u :dialog :shift-u-follow
-   :shift-u-no-opt :show-2d :front-end :multi :mspeedup :windowed
-   :sound-mute :view-override :visible :text-entry])
+  [:game :replay :paused :shift-u :dialog :shift-u-follow :shift-u-no-opt
+   :show-2d :front-end :multi :mspeedup :windowed :sound-mute :view-override
+   :visible :text-entry])
+
+(def ^:private ALC_CARS
+  ["XFG" "XRG" "XRT" "RB4" "FXO" "LX4" "LX6" "MRT" "UF1" "RAC" "FZ5" "FOX" "XFR" "UFR" "FO8" "FXR" "XRR" "FZR" "BF1" "FBM"])
+
+(def ^:private LCS_SWITCHES
+  [:set-signals :set-flash :headlights :horn :siren])
+
+(def ^:private VIEW_IDENTIFIERS
+  [:follow :heli :cam :driver :custom])
 
 (def ^:private BODY_PARSERS
-  {:sta
-   #:body{:flags (partial flags/parse STA_FLAGS)}})
+  {:cch
+   #:body{:camera (partial nth VIEW_IDENTIFIERS)}
+
+   :small
+   {:alc #:body{:cars (partial flags/parse ALC_CARS)}
+    :lcs #:body{:switches (partial flags/parse LCS_SWITCHES)}
+    :tms #:body{:stop (partial nth [:carry-on :stop])}
+    :vta #:body{:action (partial nth [:none :end :restart :qualify])}}
+
+   :sta
+   #:body{:flags (partial flags/parse STA_FLAGS)
+          :in-game-cam (partial nth VIEW_IDENTIFIERS)
+          :race-in-progress (partial nth [:no-race :race :qualifying])
+          :race-laps parse-race-laps
+          :wind (partial nth [:off :weak :strong])}})
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Private parse functions
 
 (defn- parse-header-data
   "Returns header with `:header/data` parsed.
-   ```clojure
-  (parse-data #:header{:type :tiny :data 2})
-  => #:header{:type :tiny :data :close}
-  ```"
+   `(parse-data #:header{:type :tiny :data 2}) => #:header{:type :tiny :data :close}`"
   [{:header/keys [type] :as header}]
   (let [data-enum (get DATA type)]
     (cond-> header
       data-enum (update :header/data (partial nth data-enum)))))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Public parse functions
+
 (defn header
-  "Returns header with `:header/type` and `:header/data` parsed. Returns `nil` when header or type are falsey.
-   ```clojure
-  (parse-data #:header{:type 3 :data 2})
-  => #:header{:type :tiny :data :close}
-  ```"
+  "Returns header with `:header/type` and `:header/data` parsed. Returns `nil`
+   when header or type are falsey.
+   `(header #:header{:type 3 :data 2}) => #:header{:type :tiny :data :close}`"
   [{:header/keys [type] :as header}]
   (when (and header type)
     (-> header
         (update :header/type (partial nth TYPES))
         (parse-header-data))))
 
-;; TODO: Implement this
-(defn body [{:header/keys [type] :as packet}]
-  (let [parsers (get BODY_PARSERS type)]
-    (println parsers)
-    (println packet)
+(defn body
+  "Returns packet with it's :body/keys parsed, based on BODY_PARSERS
+   ```clojure
+  (body {:header/type :small :header/data :vta :body/unique-connection-id 1 :body/action 1})
+  => {:header/type 4 :body/unique-connection-id 1 :body/action :end}```"
+  [{:header/keys [type data] :as packet}]
+  (let [parsers (if-let [parsers (and (#{:small} type)
+                                      (or (get-in BODY_PARSERS [type data]) {}))]
+                  parsers
+                  (get BODY_PARSERS type))]
     (cond-> packet
       parsers (u/map-kv parsers))))
 
