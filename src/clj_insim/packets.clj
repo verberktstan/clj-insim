0b09c1d18e300c6976b6ba767efda82a724e91db
verberktstan <stan@brightin.nl>
2021-06-15 07:56:00 +0200

    Clean up packets ns

--- src/clj_insim/packets.clj
+++ src/clj_insim/packets.clj
@@ -9,9 +9,10 @@
            iname "clj-insim"
            insim-version 8
            prefix \!}}]
-   {:header/size 44 :header/type :isi :header/request-info 1 :header/data 0
-    :body/udp-port 0 :body/is-flags 0 :body/insim-version insim-version
-    :body/prefix prefix :body/interval 0 :body/admin admin :body/iname iname}))
+   (merge
+    #:header{:size 44 :type :isi :request-info 1 :data 0}
+    #:body{:udp-port 0 :is-flags 0 :insim-version insim-version :prefix prefix
+           :interval 0 :admin admin :iname iname})))
 
 (defn tiny
   ([]
@@ -19,4 +20,4 @@
   ([{:keys [request-info data]
       :or {request-info 0
            data :none}}]
-   {:header/size 4 :header/type :tiny :header/request-info request-info :header/data data}))
+   #:header{:size 4 :type :tiny :request-info request-info :data data}))
