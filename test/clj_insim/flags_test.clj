5928b0c8bb74a6fc1c3d1214a6d792e2e17bf4fd
verberktstan <stan@brightin.nl>
2021-06-17 07:44:20 +0200

    Refactor flags parsing to higher order functions and move data

--- test/clj_insim/flags_test.clj
+++ test/clj_insim/flags_test.clj
@@ -9,24 +9,24 @@
 
 (deftest parse-test
   (testing "parse"
-    (let [coll [:a :b :c]]
+    (let [parse (sut/parse [:a :b :c])]
       (are [x y] (= x y)
-        #{:a} (sut/parse coll 1)
-        #{:b} (sut/parse coll 2)
-        #{:a :b} (sut/parse coll 3)
-        #{:c} (sut/parse coll 4)
-        #{:a :c} (sut/parse coll 5)
-        #{:b :c} (sut/parse coll 6)
-        #{:a :b :c} (sut/parse coll 7)))))
+        #{:a} (parse 1)
+        #{:b} (parse 2)
+        #{:a :b} (parse 3)
+        #{:c} (parse 4)
+        #{:a :c} (parse 5)
+        #{:b :c} (parse 6)
+        #{:a :b :c} (parse 7)))))
 
 (deftest unparse-test
   (testing "unparse"
-    (let [coll [:a :b :c]]
+    (let [unparse (sut/unparse [:a :b :c])]
       (are [x y] (= x y)
-        1 (sut/unparse coll #{:a})
-        2 (sut/unparse coll #{:b})
-        3 (sut/unparse coll #{:a :b})
-        4 (sut/unparse coll #{:c})
-        5 (sut/unparse coll #{:a :c})
-        6 (sut/unparse coll #{:b :c})
-        7 (sut/unparse coll #{:a :b :c})))))
+        1 (unparse #{:a})
+        2 (unparse #{:b})
+        3 (unparse #{:a :b})
+        4 (unparse #{:c})
+        5 (unparse #{:a :c})
+        6 (unparse #{:b :c})
+        7 (unparse #{:a :b :c})))))
