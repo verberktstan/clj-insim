50db9bb3dd9d7c0b5bc5781334fe1b415a63434d
verberktstan <stan@brightin.nl>
2021-06-26 21:39:47 +0200

    Refactor unknown codec and fix packets/plc

--- src/clj_insim/write.clj
+++ src/clj_insim/write.clj
@@ -4,14 +4,17 @@
             [clj-insim.parse :as parse]
             [marshal.core :as m]))
 
+(defn- unknown-codec-fn [size]
+  (m/struct :body/unkown (m/ascii-string (- size 4))))
+
 (defn packet!
   "Parse the packet as instruction (packet to send to LFS) and write it to the
    output-stream."
   [output-stream {:header/keys [size type] :as packet}]
   {:pre [(packet/parsed? packet)]}
   (let [instruction (parse/instruction packet)
-        body-codec (get codecs/body type #(m/struct :body/unkown (m/ascii-string (- size 4))))]
+        body-codec-fn (get codecs/body type unknown-codec-fn)]
     (m/write output-stream codecs/header instruction)
     (when (> size 4)
-      (m/write output-stream (body-codec packet) instruction))
+      (m/write output-stream (body-codec-fn packet) instruction))
     (.flush output-stream)))
