c47479e67c46d9a37a6b9109fc6c33af12d7a41f
verberktstan <stan@brightin.nl>
2021-06-27 15:54:12 +0200

    Separate concerns (parsing/side-effect) in write ns, add test

--- src/clj_insim/write.clj
+++ src/clj_insim/write.clj
@@ -4,17 +4,29 @@
             [clj-insim.parse :as parse]
             [marshal.core :as m]))
 
-(defn- unknown-codec-fn [size]
-  (m/struct :body/unkown (m/ascii-string (- size 4))))
+(defn- unknown-codec [{:header/keys [size]}]
+  (when (> size 4)
+    (m/struct :body/unkown (m/ascii-string (- size 4)))))
 
-(defn packet!
-  "Parse the packet as instruction (packet to send to LFS) and write it to the
-   output-stream."
-  [output-stream {:header/keys [size type] :as packet}]
+(defn- prepare
+  "Prepare the packet by parsing the packet into a raw packet and determine the
+   body codec."
+  [{:header/keys [size type] :as packet}]
   {:pre [(packet/parsed? packet)]}
-  (let [instruction (parse/instruction packet)
-        body-codec-fn (get codecs/body type unknown-codec-fn)]
-    (m/write output-stream codecs/header instruction)
-    (when (> size 4)
-      (m/write output-stream (body-codec-fn packet) instruction))
-    (.flush output-stream)))
+  {:instruction (parse/instruction packet)
+   :body-codec ((get codecs/body type unknown-codec) packet)})
+
+(defn- write-instruction!
+  "Write raw instruction packet to output stream."
+  [output-stream body-codec instruction]
+  {:pre [(packet/raw? instruction)]}
+  (m/write output-stream codecs/header instruction)
+  (when body-codec
+    (m/write output-stream body-codec instruction))
+  (.flush output-stream))
+
+(defn instruction
+  "Prepares the packet and write it to the output stream."
+  [output-stream packet]
+  (let [{:keys [body-codec instruction]} (prepare packet)]
+    (write-instruction! output-stream body-codec instruction)))
